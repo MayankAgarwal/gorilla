f4e96559c9e4ee7da39e81f7911c1e97aa2e8981
Mayank Agarwal <Mayank.Agarwal@ibm.com>
2024-07-11 18:05:00 -0400

    update organization

--- berkeley-function-call-leaderboard/eval_checker/eval_runner_helper.py
+++ berkeley-function-call-leaderboard/eval_checker/eval_runner_helper.py
@@ -376,7 +376,7 @@ MODEL_METADATA_MAPPING = {
     "ibm-granite/granite-20b-functioncalling": [
         "Granite-20b-FunctionCalling (FC)",
         "https://huggingface.co/ibm-granite/granite-20b-functioncalling",
-        "IBM Research",
+        "IBM",
         "Apache-2.0",
     ],
     "THUDM/glm-4-9b-chat": [
@@ -645,7 +645,10 @@ def api_status_sanity_check_rest():
             errors.append((data, status))
 
     if correct_count != len(ground_truth_replaced):
-        raise BadAPIStatusError(errors, f"{len(ground_truth_replaced) - correct_count} / {len(ground_truth_replaced)}")
+        raise BadAPIStatusError(
+            errors,
+            f"{len(ground_truth_replaced) - correct_count} / {len(ground_truth_replaced)}",
+        )
 
 
 def api_status_sanity_check_executable():
@@ -669,7 +672,9 @@ def api_status_sanity_check_executable():
             errors.append((data, status))
 
     if correct_count != len(ground_truth):
-        raise BadAPIStatusError(errors, f"{len(ground_truth) - correct_count} / {len(ground_truth)}")
+        raise BadAPIStatusError(
+            errors, f"{len(ground_truth) - correct_count} / {len(ground_truth)}"
+        )
 
 
 def display_api_status_error(rest_error, executable_error, display_success=False):
@@ -681,17 +686,23 @@ def display_api_status_error(rest_error, executable_error, display_success=False
     RED_FONT = "\033[91m"
     RESET = "\033[0m"
 
-    print(f"\n{RED_FONT}{'-' * 18} Executable Categories' Error Bounds Based on API Health Status {'-' * 18}{RESET}\n")
+    print(
+        f"\n{RED_FONT}{'-' * 18} Executable Categories' Error Bounds Based on API Health Status {'-' * 18}{RESET}\n"
+    )
 
     if rest_error:
-        print(f"❗️ Warning: Unable to verify health of executable APIs used in executable test category (REST). Please contact API provider.\n")
+        print(
+            f"❗️ Warning: Unable to verify health of executable APIs used in executable test category (REST). Please contact API provider.\n"
+        )
         print(f"{rest_error.error_rate} APIs affected:\n")
         for data, status in rest_error.errors:
             print(f"  - Test Case: {data['ground_truth']}")
             print(f"    Error Type: {status['error_type']}\n")
 
     if executable_error:
-        print(f"❗️ Warning: Unable to verify health of executable APIs used in executable test categories (Non-REST). Please contact API provider.\n")
+        print(
+            f"❗️ Warning: Unable to verify health of executable APIs used in executable test categories (Non-REST). Please contact API provider.\n"
+        )
         print(f"{executable_error.error_rate} APIs affected:\n")
         for data, status in executable_error.errors:
             print(f"  - Test Case: {data['ground_truth'][0]}")
